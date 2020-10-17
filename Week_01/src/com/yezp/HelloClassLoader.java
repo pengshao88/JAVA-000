89a51725818d7ff0900e557ea754a1690fb095ee
pengshao88 <yezhipengkobe@163.com>
2020-10-17 17:30:17 +0800

    homework

--- Week_01/src/com/yezp/HelloClassLoader.java
+++ Week_01/src/com/yezp/HelloClassLoader.java
@@ -1,4 +1,67 @@
 package com.yezp;
 
-public class HelloClassLoader {
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class HelloClassLoader extends ClassLoader {
+
+    public static void main(String[] args) {
+        try {
+            Class clazz = new HelloClassLoader().findClass("Hello");
+            if (clazz == null)
+                return;
+
+            Object obj = clazz.newInstance();
+            Method method = clazz.getMethod("hello");
+            method.invoke(obj);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) {
+        String filePath = System.getProperty("user.dir") + File.separator + "resources";
+        String fileName = filePath + File.separator + "hello.xlass";
+
+        File file = new File(fileName);
+        byte[] bytes;
+        try {
+            FileInputStream fs = new FileInputStream(file);
+            long size = file.length();
+            if (size == 0L) {
+                return null;
+            }
+            if (size > Integer.MAX_VALUE) {
+                return null;
+            }
+
+            int count = (int) size;
+            bytes = new byte[count];
+            fs.read(bytes);
+            convert(bytes, count);
+
+            fs.close();
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void convert(byte[] bs, int len) {
+        for (int i = 0; i < len; i++) {
+            bs[i] = ((byte) (bs[i] ^ 0xFFFFFFFF));
+        }
+    }
 }
--- Week_01/src/com/yezp/Main.java
+++ Week_01/src/com/yezp/Main.java
@@ -1,4 +1,21 @@
 package com.yezp;
 
 public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 30;
+
+        int add = x + y;
+        int sub = add - y;
+        int mutil = add * sub;
+        int divide = mutil / sub;
+
+        if (divide > 0) {
+            for ( int i = 0; i < divide; i++) {
+                i++;
+            }
+        }
+    }
+
 }
