1919897f46c6915212a96587f3269bcb780344b0
pengshao88 <yezhipengkobe@163.com>
2020-10-25 17:36:15 +0800

    v1.0

---
+++ Week_02/week02_homework/src/main/java/com/yezp/Main.java
@@ -0,0 +1,33 @@
+package com.yezp;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.yezp.util.HttpClientUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JSONObject json;
+        String url = "http://localhost:8801";
+        Map<String, String> paraMap = new HashMap<String, String>();
+
+        try {
+            String result = HttpClientUtil.sendPost(url, paraMap);
+            if (result == null || result.length() == 0) {
+                System.out.println("result is empty.");
+            }
+
+            json = JSONObject.parseObject(result);
+            System.out.println(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
